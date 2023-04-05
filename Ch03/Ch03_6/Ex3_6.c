fabb6da713241c9df0d54e7311cfe6df5c36abce
HeeJun <unijun0109@gmail.com>
2023-04-05 14:21:47 +0900

    5th Weeks

---
+++ Ch03/Ch03_6/Ex3_6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "addPoly.h"
+
+int main()
+{
+	polynomial A = { 3, { 4, 3, 5, 0 } };
+	polynomial B = { 4, { 3, 1, 0, 2, 1 } };
+	polynomial C;
+	
+	C = addPoly(A, B);   // 다항식 A, B에 대한 덧셈을 수행하기 위해 addPoly 함수 호출
+
+	printf("\n A(x) = ");
+	printPoly(A);   // 다항식 A 출력
+	
+	printf("\n B(x) = ");
+	printPoly(B);   // 다항식 B 출력
+
+	printf("\n C(x) = ");
+	printPoly(C);   // 다항식 C 출력
+	getch();
+
+	return 0;
+}
---
+++ Ch03/Ch03_6/addPoly.h
@@ -0,0 +1,12 @@
+#pragma once
+#define MAX(a, b) ((a > b) ? a : b)
+#define MAX_DEGREE 50
+
+typedef struct   // 구조체 polynomial 정의
+{
+	int degree;   // 다항식의 차수를 저장할 변수 선언
+	float coef[MAX_DEGREE];   // 다항식의 각 항의 계수를 저장할 1차원 배열 선언
+} polynomial;
+
+polynomial addPoly(polynomial A, polynomial B);
+void printPoly(polynomial P);
